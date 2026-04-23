3576dad50ea3baf86d636051741ff68532a80913
VanrougeK <ochoavania795@gmail.com>
2026-04-22 20:09:14 -0600

    Modified
    Se modifico el archivo para conectarlo con el frontend aunque aun falta ya que tiene errores

--- index.js
+++ index.js
@@ -1,42 +1,52 @@
 // importar librerias
 const express = require("express")
-
+const cors = require("cors")
+const mysql = require("mysql2")
 // instanciar la app
 const app = express()
-
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"gatitos"
+})
+db.connect(err => {
+    if(err) {
+        console.error("Error mysql:", err)
+    } else {
+        console.log("Conectado a mysql")
+    }
+})
 // aca le decimos middelware para json
 app.use(express.json())
+app.use(cors())
 
 // configurar puerto
 const PORT = 3014
 
-app.get("/", (req, res) => {
-    res.send("Hola mundo desde backend")
-})
+app.post(`/scores`, (req, res) => {
+    const {user_id, puntuacion} = req.body
 
-let users = [{id:1, nombre:`golda`}]
-app.get(`/users`, (req, res) => res.json(users))
-app.post(`/users`, (req, res) => {
-    const nuevo = {id:users.length+1, ...req.body}
-    users.push(nuevo);
-    res.status(201).json(nuevo)
-})
-
-app.get(`/users/:id`, (req, res) => {
-    const idDelete = parseInt(req.params.id)
-    const index = users.findIndex (u => u.id === idDelete)
-    if(index === -1) {
-        return res.status(404).json({
-            error: "Usuario no encontrado",
-            message: `No existe un usuario con el id ${idDelete}`
+    db.query(
+        "INSERT INTO users (username, password) VALUES (?, ?)",
+        [nombre, puntuacion],
+        (err, result) => {
+            if(err) return res.status(500).json(err) 
+                res.status(201).json({
+            nombre, 
+            puntuacion
         })
     }
-    const userElimated = users.splice(index,1)
+)
+})
 
-    res.json ({
-        message: "Usuario eliminado",
-        users:userElimated[0]
-    })
+app.get("/ranking", (req, res) => {
+    db.query(`
+        SELECT users.username, MAX(scores.puntuacion) AS mejor FROM scores JOIN users ON users.id = scores.user_id GROUP by users.id ORDER BY mejor DESC`,
+        (err, results) => {
+            if (err) return res.status(500).json(err)
+                res.json(results)
+        })
 })
 
 app.listen(PORT, () => {
