ff2cd5ecc4cdfa4a458c2ca4f237c85678318477
VanrougeK <ochoavania795@gmail.com>
2026-05-22 14:14:40 -0600

    Modified skinscontroller

--- controllers/skinsController.js
+++ controllers/skinsController.js
@@ -1,19 +1,18 @@
 const db = require("../config/db");
 
-const obtenerSkins = (req, res) => {
-  db.query(
-    `SELECT id, nombre, nombre, requisito_puntos FROM skins`,
-    (err, results) => {
-      if (err) {
-        console.log(err);
+const obtenerSkins = async (req, res) => {
+  try {
+    const [results] = await db.query (
+      `SELECT id, nombre, nombre, requisito_puntos FROM skins`
+    )
+    res.json(results)
+  } catch (err) {
+console.log(err);
         return res.status(500).json({
           error: "Error al obtener skins",
-        });
-      }
-      res.json(results);
-    },
-  );
-};
+        })
+  }
+}
 const cambiarSkin = (req, res) => {
   res.json({
     mensaje: "Skin cambiada",
