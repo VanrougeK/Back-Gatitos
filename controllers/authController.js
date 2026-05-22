28ceb7528635e2223d8f0e8132cc7287f3770f76
VanrougeK <ochoavania795@gmail.com>
2026-05-21 18:41:20 -0600

    Fix authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,61 +1,70 @@
-const db = require("../config/db")
-const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken")
+const db = require("../config/db");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res) => {
-    const { usuario, password } = req.body
-    if(!usuario || !password){
-        return res.status(400).json({
-            error: "Faltan datos"
-        })
+  const { usuario, password } = req.body;
+  if (!usuario || !password) {
+    return res.status(400).json({
+      error: "Faltan datos",
+    });
+  }
+
+  try {
+    const hash = await bcrypt.hash(password, 10);
+
+    await db.query("INSERT INTO users(username, password) VALUES (?, ?)", [
+      usuario,
+      hash,
+    ]);
+
+    return res.json({
+      mensaje: "Cuenta creada",
+    });
+  } catch (err) {}
+  (err) => {
+    console.log("Error de registro: ", err);
+    return res.status(500).json({
+      error: "Usuario ya existente o error en el serv.",
+    });
+  };
+};
+
+exports.login = async (req, res) => {
+  const { usuario, password } = req.body;
+
+  try {
+    const [results] = await db.query("SELECT * FROM users WHERE username = ?", [
+      usuario,
+    ]);
+
+    if (results.length === 0) {
+      return res.status(400).json({
+        error: "Usuario no encontrado",
+      });
     }
 
-    const hash = await bcrypt.hash(password, 10)
+    const user = results[0];
+    const valido = await bcrypt.compare(password, user.password);
 
-    db.query(
-        "INSERT INTO users(username, password) VALUES (?, ?)", [usuario, hash], (err) => {
-            if(err){
-                console.log(err)
-                return res.status(500).json({
-                    error: "Usuario ya existe"
-                })
-            }
-            res.json({
-                mensaje: "Cuenta creada"
-            })
-        }
-    )
-}
+    if (!valido) {
+      return res.status(400).json({
+        error: "Contraseña incorrecta",
+      });
+    }
 
-exports.login = (req, res) => {
-    const { usuario, password } = req.body
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
 
-    db.query(
-        "SELECT * FROM users WHERE username = ?", [usuario], async (err, results) => {
-            if(err) {
-                console.log(err)
-                return res.status(500).json({
-                    error: "Error del servidor"
-                })
-            }
-            if(results.length === 0) {
-                return res.status(400).json({
-                    error: "Usuario no encontrado"
-                })
-            }
-
-            const user = results[0]
-            const valido = await bcrypt.compare(password, user.password)
-
-            if(!valido){
-                return res.status(400).json({
-                    error: "Contraseña incorrecta"
-                })
-            }
-
-            const token = jwt.sign({id: user.id}, process.env.JWT_SECRET)
-
-            res.json({token, usuario:user.username, puntos:user.highscore, skin: user.skin_actual})
-        }
-    )
-}
+    return res.json({
+      token,
+      usuario: user.username,
+      puntos: user.highscore,
+      skin: user.skin_actual,
+    });
+  } catch (err) {
+    console.log("Error en login: ", err);
+    return res.status(500).json({
+      error: "Error del servidor",
+    });
+  }
+};
