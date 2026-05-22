3a2944fbf92735dfd9c0511520bc380c89d88f19
VanrougeK <ochoavania795@gmail.com>
2026-05-21 18:14:42 -0600

    fix ssl aiven

--- config/db.js
+++ config/db.js
@@ -8,6 +8,7 @@ const db = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 5,
   queueLimit: 0,
+  ssl: {rejectUnauthorized: false}
 });
 
 db.getConnection((err, connection) => {
@@ -18,4 +19,4 @@ db.getConnection((err, connection) => {
     connection.release();
   }
 });
-module.exports = db;
+module.exports = db.promise;
