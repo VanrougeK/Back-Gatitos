c104c6d66b808f9539c9eeb2c2f9dfe4ad61a523
VanrougeK <ochoavania795@gmail.com>
2026-05-21 19:45:36 -0600

    .

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const ranking = require("./routes/ranking")
 const skinsRoutes = require("./routes/skins")
 
 
-app.use(cors())
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}))
 app.use(express.json())
 
 app.use("/auth", authRoutes)
